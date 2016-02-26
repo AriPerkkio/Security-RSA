4df26eee14e8beffe7f857789c6c655adf6670b8
AriPerkkio <t3pear00@students.oamk.fi>
2016-02-26 20:49:42 +0000

    GUI Started

--- SecurityAssignment/src/control.java
+++ SecurityAssignment/src/control.java
@@ -1,58 +1,136 @@
 import java.math.BigInteger;
 import java.security.SecureRandom;
 import java.util.logging.Logger;
+import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 
 public class control extends JFrame{
 	
-    public control(String _text, String panel){
-        super(_text);
+    static final JButton btnGetKeys = new JButton("Generate keys");
+    static final JTextField publicKeyText = new JTextField("Public key: ");
+    static final JTextField exponentText = new JTextField("Exponent: ");
+    static final JTextField privateKeyText = new JTextField("Private key:");
+	
+    static final JButton btnEncrypt = new JButton("Encrypt message");
+    static final JTextField inputMessage = new JTextField("Write your message");
+    
+    static final JButton btnDecrypt = new JButton("Decrypt message");
 
+    
+
+    public static class RSAKeys extends JPanel{
+    	
+	    static RSAKeys instance;
+
+	    public RSAKeys(){
+	    	instance = this;
+	    }
+	    
+      @Override
+      public void paintComponent(Graphics g){  	        	    
+    	    btnGetKeys.addActionListener(new ActionListener(){
+    	    	public void actionPerformed(ActionEvent e) {
+    	    		publicKeyText.setBounds(0, 160, 0, 30);
+    	    		publicKeyText.setSize(400, 30);
+    	    		RSAKeys.instance.add(publicKeyText);
+    	    		
+    	    	    
+    	    	    exponentText.setBounds(0, 200, 0, 30);
+    	    	    exponentText.setSize(400, 30);
+    	    	    RSAKeys.instance.add(exponentText);
+    	    	    
+    	    	    privateKeyText.setBounds(0, 250, 0, 30);
+    	    	    privateKeyText.setSize(400, 30);
+    	    	    RSAKeys.instance.add(privateKeyText);
+    	    		
+    	    		int bitCount = 30;
+    	    		PrimeNumber p = new PrimeNumber(bitCount);
+    	    		PrimeNumber q = new PrimeNumber(bitCount);
+    	    		BigInteger keys[] = calcKeys(p, q);	
+    	    		publicKeyText.setText("Public key ("+keys[0].toString(2).length()+" bits) : "+keys[0]);
+    	    		exponentText.setText("Exponent ("+keys[1].toString(2).length()+" bits): "+keys[1]);
+    	    		privateKeyText.setText("Private key ("+keys[2].toString(2).length()+" bits) : "+keys[2]);
+    	    	}});
+    	    this.add(btnGetKeys);
+       }
+    }
+    
+    static class Encrypt extends JPanel{
+    
+    	static Encrypt instance;
+    	public Encrypt(){
+    		instance = this;
+    		
+    	}
+    	
+    	@Override
+        public void paintComponent(Graphics g){
+    	    
+    		
+    		btnEncrypt.setBounds(100,60,100,30);
+    	    btnEncrypt.setSize(new Dimension(200, 50));
+    	    this.add(btnEncrypt);
+    	    
+        }
+    }
+    class Decrypt extends JPanel{
+    	@Override
+        public void paintComponent(Graphics g){
+        	btnDecrypt.setBounds(100,60,100,30);
+        	btnDecrypt.setSize(new Dimension(200, 50));
+    	    this.add(btnDecrypt);
+        }        
+    }
+	
+    public control(String _text, String panel){
+        super(_text); // Set title
 
         switch(panel){
-        	case "default":
-        		setContentPane(new DrawPane());
+        	case "rsakeys":
+        		setContentPane(new RSAKeys());
         	break;
         	
         	case "encrypt":
-        		setContentPane(new DrawEncrypt());
+        		setContentPane(new Encrypt());
+        	break;
+        	
+        	case "decrypt":
+        		setContentPane(new Decrypt());
         	break;
         	
         }
-        
-
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
         setSize(400, 400);
-
         setVisible(true); 
    }
-
-    //create a component that you can actually draw on.
-    class DrawPane extends JPanel{
-      public void paintComponent(Graphics g){
-        //draw on g here e.g.
-         g.fillRect(0,0,400,400);
-       }
-   }
-    
-    class DrawEncrypt extends JPanel{
-        public void paintComponent(Graphics g){
-          
-           
-         }
-     }
 	
 	public static void main(String[] args) {
 		
-		new control("RSA Keys", "default");
-		new control("Encrypt", "encrypt");
-		new control("Decrypt", "default");
+		JFrame rsa = new control("RSA Keys", "rsakeys");
+		final JFrame encrypt = new control("Encrypt", "encrypt");
+		final JFrame decrypt = new control("Decrypt", "decrypt");
+		rsa.setSize(400, 400);
+		encrypt.setSize(400,400);
+		decrypt.setSize(400,400);
+		encrypt.move(500,50);
+		decrypt.move(1000, 50);
 		
+		rsa.addWindowListener(new WindowAdapter() {
+	         public void windowClosing(WindowEvent windowEvent){
+	             System.exit(0);
+	          }        
+	       });    
+	 	/*
 		long startTime = System.nanoTime();
 		String secretMessage = "This is my secret message which will be encrypted.";
 		int bitCount = 32;
@@ -89,6 +167,7 @@ public class control extends JFrame{
 		long duration = (endTime - startTime)/1000000000;  
 		System.out.println("\n\nDuration: "+duration+" s");
 		//System.exit(0); 
+	*/
 	}
 	
 	// Returns: [0] = gcd - [1] = factor for y
