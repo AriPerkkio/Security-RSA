c596843c5354fb7e08544dec4d0499a1b76cf0f7
AriPerkkio <t3pear00@students.oamk.fi>
2016-02-27 17:42:58 +0000

    File encryption added

--- SecurityAssignment/src/control.java
+++ SecurityAssignment/src/control.java
@@ -1,5 +1,6 @@
 import java.math.BigInteger;
 import java.security.SecureRandom;
+import java.util.Scanner;
 import java.util.logging.Logger;
 import java.awt.Dimension;
 import java.awt.Graphics;
@@ -7,6 +8,12 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.lang.reflect.Array;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -18,10 +25,14 @@ public class control extends JFrame{
 	
 	static BigInteger keys[] = new BigInteger[3];
 	static BigInteger encryptedMessage[] = new BigInteger[5000];
+	static BigInteger encryptedFile[] = new BigInteger[50000000];
+	static BigInteger encryptedInputFile[];
+	static BigInteger decryptedMessage[] = new BigInteger[5000];
+	static BigInteger decryptedFile[] = new BigInteger[50000000];
 
 	// Window #1 for generating keys 	
     static final JButton btnGetKeys = new JButton("Generate keys");
-    static final JTextField bitCountText = new JTextField("Bit count");
+    static final JTextField bitCountText = new JTextField("30");
     static final JTextField publicKeyText = new JTextField("Public key: ");
     static final JTextField exponentText = new JTextField("Exponent: ");
     static final JTextField privateKeyText = new JTextField("Private key:");
@@ -30,8 +41,11 @@ public class control extends JFrame{
     static final JButton btnEncrypt = new JButton("Encrypt message");
     static final JTextField publicKeyTextTwo = new JTextField("Public key: ");
     static final JTextField exponentTextTwo = new JTextField("Exponent: ");
-    static final JTextField inputMessageText = new JTextField("Write your message");
+    static final JTextField inputMessageText = new JTextField("My message");
     static final JTextField encryptedMessageText = new JTextField("<Encrypted message>");
+    static final JButton btnEncryptFile = new JButton("Encrypt file");
+    static final JTextField inputFilePathText = new JTextField("src/plaintextfile");
+    static final JTextField outputFilePathText = new JTextField("src/encryptedfile");
     
     // Window #3 for decrypting message	    
     static final JButton btnDecrypt = new JButton("Decrypt message");
@@ -40,6 +54,9 @@ public class control extends JFrame{
     static final JTextField inputMessageTextTwo = new JTextField("<Your message>");
     static final JTextField encryptedMessageTextTwo = new JTextField("<Encrypted message>");
     static final JTextField decryptedMessageText = new JTextField("<Encrypted message>");
+    static final JButton btnDecryptFile = new JButton("Decrypt file");
+    static final JTextField inputFilePathTextTwo = new JTextField("src/encryptedfile");
+    static final JTextField outputFilePathTextTwo = new JTextField("src/decryptedfile");
     
 
     public static class RSAKeys extends JPanel{
@@ -56,7 +73,7 @@ public class control extends JFrame{
   			btnGetKeys.setSize(new Dimension(200, 50));
   			bitCountText.setBounds(100,0,100,20);
   			RSAKeys.instance.add(bitCountText);
-    	    btnGetKeys.addActionListener(new ActionListener(){
+    	    btnGetKeys.addActionListener(new ActionListener(){ // Key Generate keys
     	    	public void actionPerformed(ActionEvent e) {
     	    		publicKeyText.setBounds(0, 160, 0, 30);
     	    		publicKeyText.setSize(400, 30);
@@ -92,6 +109,14 @@ public class control extends JFrame{
     	    		inputMessageText.setSize(400, 30);
     	    		Encrypt.instance.add(inputMessageText);
     	    		
+    	    		inputFilePathText.setBounds(0, 400, 0, 30);
+    	    		inputFilePathText.setSize(400, 30);
+    	    		Encrypt.instance.add(inputFilePathText);
+    	    		
+    	    		outputFilePathText.setBounds(0, 440, 0, 30);
+    	    		outputFilePathText.setSize(400, 30);
+    	    		Encrypt.instance.add(outputFilePathText);
+    	    		
     	    		privateKeyTextTwo.setBounds(0, 160, 0, 30);
     	    		privateKeyTextTwo.setSize(400, 30);
     	    		privateKeyTextTwo.setText("Private key ("+keys[2].toString(2).length()+" bits) : "+keys[2]);
@@ -111,8 +136,7 @@ public class control extends JFrame{
     
     	static Encrypt instance;
     	public Encrypt(){
-    		instance = this;
-    		
+    		instance = this;	
     	}
     	
     	@Override
@@ -120,7 +144,7 @@ public class control extends JFrame{
     	    
     		btnEncrypt.setBounds(100,60,100,30);
     	    btnEncrypt.setSize(new Dimension(200, 50));
-    	    btnEncrypt.addActionListener(new ActionListener(){
+    	    btnEncrypt.addActionListener(new ActionListener(){ // Key Encrypt message
     	    	public void actionPerformed(ActionEvent e) {
     	    		encryptedMessageText.setBounds(0, 280, 0, 30);
     	    		encryptedMessageText.setSize(400, 30);
@@ -140,6 +164,59 @@ public class control extends JFrame{
     	    }); 
     	    this.add(btnEncrypt);
     	    
+    	    btnEncryptFile.setBounds(100,340,100,30);
+    	    btnEncryptFile.setSize(new Dimension(200, 50)); 
+    	    btnEncryptFile.addActionListener(new ActionListener(){ // Key Encrypt File
+    	    	public void actionPerformed(ActionEvent e) {
+    	    		String inputFileString = "";
+    	    		// Read in file content
+    	    		try {
+    	    			FileReader inputFile = new FileReader(inputFilePathText.getText());
+    	    			Scanner in = new Scanner(inputFile);
+    	    			while(in.hasNext())
+    	    				inputFileString = inputFileString + in.next()+ " ";
+        	    		inputFile.close();
+        	    		in.close();
+    	    		} catch (FileNotFoundException ee) {
+    	    			// TODO Auto-generated catch block
+    	    			ee.printStackTrace();
+    	    			System.exit(0);
+    	    		} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+    	    		
+    	    		// Encrypt content
+    	    		encryptedFile = encrypt(inputFileString, keys[0], keys[1]);
+    	    		String encryptedFileString = "";
+    	    		for(int i=0;i<encryptedFile.length;i++){
+    	    			encryptedFileString += encryptedFile[i] + ",";
+    	    		}
+    	    		encryptedFileString += "0";
+    	    		// Write encrypted content to file
+    				PrintWriter printer;
+    				File outputFile  = new File(outputFilePathText.getText());
+    				try {
+    					printer = new PrintWriter(outputFile);
+    					printer.write(encryptedFileString);
+        				printer.flush();	
+        				printer.close();
+    				} catch (FileNotFoundException ee) {
+    					// TODO Auto-generated catch block
+    					ee.printStackTrace();
+    				}
+    				
+    	    		inputFilePathTextTwo.setBounds(0, 400, 0, 30);
+    	    		inputFilePathTextTwo.setSize(400, 30);
+    	    		Decrypt.instance.add(inputFilePathTextTwo);
+    	    		outputFilePathTextTwo.setBounds(0, 440, 0, 30);
+    	    		outputFilePathTextTwo.setSize(400, 30);
+    	    		Decrypt.instance.add(outputFilePathTextTwo);
+    	    		
+    	    	}
+    	    });
+    	    	
+    	    this.add(btnEncryptFile);
         }
     }
     static class Decrypt extends JPanel{
@@ -152,22 +229,72 @@ public class control extends JFrame{
         public void paintComponent(Graphics g){
         	btnDecrypt.setBounds(100,60,100,30);
         	btnDecrypt.setSize(new Dimension(200, 50));
-        	btnDecrypt.addActionListener(new ActionListener(){
+        	btnDecrypt.addActionListener(new ActionListener(){ // Key Decrypt message
      	    	public void actionPerformed(ActionEvent e) {
      	    		decryptedMessageText.setBounds(0, 280, 0, 30);
      	    		decryptedMessageText.setSize(400, 30);
-     	    		BigInteger decryptedMessage[] = decrypt(encryptedMessage, keys[0], keys[2]);
+     	    		decryptedMessage = decrypt(encryptedMessage, keys[0], keys[2]);
      	    		String decryptedMessageString = "";
      	    		for(int i=0;i<decryptedMessage.length;i++){
      	    			decryptedMessageString+= ((char) decryptedMessage[i].intValue());			
      	    		}
      	    		decryptedMessageText.setText(decryptedMessageString);
      	    		Decrypt.instance.add(decryptedMessageText);
-     	   		
      	    	}
      	    });
      	    	
     	    this.add(btnDecrypt);
+    	    
+    	    btnDecryptFile.setBounds(100,340,100,30);
+    	    btnDecryptFile.setSize(new Dimension(200, 50));
+    	    btnDecryptFile.addActionListener(new ActionListener(){ // Key Decrypt File
+     	    	public void actionPerformed(ActionEvent e) { 
+        			String encryptedFileString = "";
+    	    		// Read in file content
+    	    		try {
+    	    			FileReader inputFile = new FileReader(inputFilePathTextTwo.getText());
+    	    			Scanner in = new Scanner(inputFile);
+    	    			while(in.hasNext()){
+    	    				encryptedFileString = encryptedFileString + in.next();
+    	    			}
+        	    		inputFile.close();
+        	    		in.close();
+    	    		} catch (FileNotFoundException ee) {
+    	    			// TODO Auto-generated catch block
+    	    			ee.printStackTrace();
+    	    			System.exit(0);
+    	    		} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}	    			
+    	    		String messageParts[] = encryptedFileString.split(",");
+    	    		encryptedInputFile = new BigInteger[messageParts.length];
+    	    		
+	    			for(int i=0;i<messageParts.length;i++)
+		    				encryptedInputFile[i] = new BigInteger(messageParts[i]);
+	    			
+	    			// Decrypt content
+     	    		decryptedFile = decrypt(encryptedInputFile, keys[0], keys[2]);
+     	    		
+     	    		String decryptedMessageString = "";
+     	    		for(int i=0;i<decryptedFile.length-1;i++) // Lengt-1 since last part is 0
+     	    			decryptedMessageString+= ((char) decryptedFile[i].intValue());			
+
+     	    		// Write decrypted content to file
+    				PrintWriter printer;
+    				File outputFile  = new File(outputFilePathTextTwo.getText());
+    				try {
+    					printer = new PrintWriter(outputFile);
+    					printer.write(decryptedMessageString);
+        				printer.flush();	
+        				printer.close();
+    				} catch (FileNotFoundException ee) {
+    					// TODO Auto-generated catch block
+    					ee.printStackTrace();
+    				}	
+     	    	}
+     	    	});
+    	    this.add(btnDecryptFile);
         }        
     }
 	
@@ -189,7 +316,7 @@ public class control extends JFrame{
         	
         }
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(400, 400);
+        setSize(400, 800);
         setVisible(true); 
    }
 	
@@ -198,9 +325,9 @@ public class control extends JFrame{
 		JFrame rsa = new control("RSA Keys", "rsakeys");
 		final JFrame encrypt = new control("Encrypt", "encrypt");
 		final JFrame decrypt = new control("Decrypt", "decrypt");
-		rsa.setSize(400, 400);
-		encrypt.setSize(400,400);
-		decrypt.setSize(400,400);
+		rsa.setSize(400, 800);
+		encrypt.setSize(400,800);
+		decrypt.setSize(400,800);
 		encrypt.move(500,50);
 		decrypt.move(1000, 50);
 		
